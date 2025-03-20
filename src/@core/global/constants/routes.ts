13106e5e7723798c8ee1bee036bea6a913d67da9
kaufon <kauanfvale@gmail.com>
2025-03-20 12:12:43 -0300

    🐛fix: fix global hooks file names

--- src/@core/global/constants/routes.ts
+++ src/@core/global/constants/routes.ts
@@ -1,9 +1,12 @@
 export const ROUTES = {
     timePunch: '/time-punch',
     history: '/history',
+
+    // Why only three routes are portuguese???
     mirror: '/espelho',
     reviews: '/revisoes',
     report: '/relatorio',
+
     workSchedule: {
         list: '/schedule',
         register: '/register-schedule',
