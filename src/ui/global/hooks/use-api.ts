81218baf7ab5a1cc63180db6239667071a8e6d57
JohnPetros <joaopedro.nc@outlook.com>
2025-03-21 09:34:20 -0300

    🖥️ ui: add workScheduleService to useApi hook

--- src/ui/global/hooks/use-api.ts
+++ src/ui/global/hooks/use-api.ts
@@ -1,8 +1,12 @@
 import { NextApiClient } from '@/api/next'
+import { WorkScheduleService } from '@/api/services/work-schedule-service'
+import { CLIENT_ENV } from '@/constants/client-env'
 
 const nextApiClient = NextApiClient()
-nextApiClient.setBaseUrl('***')
+nextApiClient.setBaseUrl(CLIENT_ENV.serverAppUrl)
 
 export function useApi() {
-  return {}
+  return {
+    workScheduleService: WorkScheduleService(nextApiClient),
+  }
 }
