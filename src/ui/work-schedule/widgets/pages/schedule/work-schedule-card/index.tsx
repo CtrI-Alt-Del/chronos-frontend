cc7b7612e233b96dc8e8bdbcf536d187fe7c01b2
Tico1606 <gabrielsoliveira1606@gmail.com>
2025-03-27 19:54:10 -0300

    📶 api: add dinamic description on the schedules cards

--- src/ui/work-schedule/widgets/pages/schedule/work-schedule-card/index.tsx
+++ src/ui/work-schedule/widgets/pages/schedule/work-schedule-card/index.tsx
@@ -1,8 +1,12 @@
+import { WorkScheduleDto } from "@/@core/work-schedule/dtos";
 import { Card, CardHeader } from "@heroui/card";
 import { Image } from "@heroui/image";
 import Link from "next/link";
+interface WorkScheduleCardProps {
+  schedule: WorkScheduleDto
+}
 
-export const WorkScheduleCard = () => {
+export const WorkScheduleCard = ({ schedule }: WorkScheduleCardProps) => {
   return (
     <Link href="" className="focus:outline-none">
       <Card className="max-w-60 w-48 border hover:bg-gray-100 transition-all cursor-pointer" shadow="none">
@@ -15,7 +19,7 @@ export const WorkScheduleCard = () => {
             width={40}
           />
           <div className="flex flex-col">
-            <p className="text-md">Fulano</p>
+            <p className="text-md font-bold">{schedule.description}</p>
           </div>
         </CardHeader>
       </Card>
