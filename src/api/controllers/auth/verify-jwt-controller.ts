6cf234071013b05e2748a52f9785d8a1f8910a58
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-03-27 15:19:16 +0000

    🛜 api: check if the user is authenticated in VerifyJwtMiddleware

--- src/api/controllers/auth/verify-jwt-controller.ts
+++ src/api/controllers/auth/verify-jwt-controller.ts
@@ -1,19 +1,18 @@
-import type { IController } from "@/@core/global/interfaces/controller"
-import type { IHttp } from "@/@core/global/interfaces/http"
-import { ROUTES } from "@/constants/routes"
+import { COOKIES } from '@/@core/global/constants/cookies'
+import type { IController } from '@/@core/global/interfaces/controller'
+import type { IHttp } from '@/@core/global/interfaces/http'
+import { ROUTES } from '@/constants/routes'
 
-const PUBLIC_ROUTES = [
-  ROUTES.auth.login,
-]
+const PUBLIC_ROUTES = [ROUTES.auth.login]
 
 export const VerifyJwtMiddleware = (): IController => {
   return {
     async handle(http: IHttp) {
       const currentRoute = http.getCurrentRoute()
-
+      const isAuthenticated = await http.hasCookie(COOKIES.jwt.key)
       const isPublicRoute = PUBLIC_ROUTES.map(String).includes(currentRoute)
 
-      if (!isPublicRoute) {
+      if (!isAuthenticated && !isPublicRoute) {
         return http.redirect(ROUTES.auth.login)
       }
 
