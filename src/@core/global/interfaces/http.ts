3900106ec13cee6caf8bf1f024259fa35cb0d056
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-03-27 15:16:28 +0000

    🛜 api: implement hasCookie of Http method using Next

--- src/@core/global/interfaces/http.ts
+++ src/@core/global/interfaces/http.ts
@@ -1,5 +1,5 @@
-import type { AccountDto } from "@/@core/auth/dtos"
-import type { ApiResponse } from "../responses"
+import type { AccountDto } from '@/@core/auth/dtos'
+import type { ApiResponse } from '../responses'
 
 export type HttpSchema = {
   body?: unknown
@@ -16,6 +16,7 @@ export interface IHttp<Schema extends HttpSchema = HttpSchema, Response = unknow
   getAccount(): Promise<AccountDto>
   setCookie(key: string, value: string, duration: number): void
   getCookie(key: string): Promise<string | null>
+  hasCookie(key: string): Promise<boolean>
   deleteCookie(key: string): Promise<void>
   hasCookie(key: string): Promise<boolean>
   pass(): ApiResponse
