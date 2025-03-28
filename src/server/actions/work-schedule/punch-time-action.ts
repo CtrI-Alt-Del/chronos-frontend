6685b009391ee4ce7552b82c86c062f176ea3749
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-03-28 13:11:09 +0000

    📟 server: add work schedule server actions

---
+++ src/server/actions/work-schedule/punch-time-action.ts
@@ -0,0 +1,20 @@
+import { CACHE } from '@/@core/global/constants'
+import type { IAction } from '@/@core/global/interfaces/action'
+import type { IActionServer } from '@/@core/global/interfaces/action-server'
+import type { IWorkScheduleService } from '@/@core/work-schedule/interfaces'
+
+type Request = {
+  timePunchLogId: string
+  time: Date
+}
+
+export const PunchTimeAction = (service: IWorkScheduleService): IAction<Request> => {
+  return {
+    async handle(actionServer: IActionServer<Request>) {
+      const { timePunchLogId, time } = actionServer.getRequest()
+      const response = await service.punchTime(timePunchLogId, time)
+      if (response.isFailure) response.throwError()
+      actionServer.resetCache(CACHE.workSchedule.todayWordayLog.key)
+    },
+  }
+}
