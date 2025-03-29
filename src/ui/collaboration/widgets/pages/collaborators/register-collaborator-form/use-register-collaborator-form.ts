668471d3a999d2071d94b5e7e566363a321ddb52
kaufon <kauanfvale@gmail.com>
2025-03-29 01:26:45 -0300

    ♻️refactor: use-register-form refactoring

--- src/ui/collaboration/widgets/pages/collaborators/register-collaborator-form/use-register-collaborator-form.ts
+++ src/ui/collaboration/widgets/pages/collaborators/register-collaborator-form/use-register-collaborator-form.ts
@@ -2,53 +2,83 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { useApi } from '../../../../../global/hooks'
+import { useToast } from '@/ui/global/hooks/use-toast'
 
 export const registerCollaboratorFormSchema = z.object({
-    name: z
-      .string({ required_error: 'Nome é obrigatório', invalid_type_error: 'Nome inválido' })
-      .min(3, { message: 'Nome deve ter pelo menos 3 caracteres' }),
-    email: z
-      .string({ required_error: 'Email é obrigatório', invalid_type_error: 'Email inválido' })
-      .email({ message: 'Email inválido' }),
-    password: z
-      .string({ required_error: 'Senha é obrigatório', invalid_type_error: 'Senha inválida' }),
-    cpf: z
-      .string({ required_error: 'CPF é obrigatório', invalid_type_error: 'CPF inválido' })
-      .regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, { message: 'CPF inválido' }),
-    role: z
-      .string({ required_error: 'Cargo é obrigatório', invalid_type_error: 'Cargo inválido' })
-      .min(1, { message: 'Cargo deve ser selecionado' }),
-    sector: z
-      .string({ required_error: 'Setor é obrigatório', invalid_type_error: 'Setor inválido' })
-      .min(1, { message: 'Setor deve ser selecionado' }),
-  });
+  name: z
+    .string({ required_error: 'Nome é obrigatório', invalid_type_error: 'Nome inválido' })
+    .min(3, { message: 'Nome deve ter pelo menos 3 caracteres' }),
+  email: z
+    .string({
+      required_error: 'Email é obrigatório',
+      invalid_type_error: 'Email inválido',
+    })
+    .email({ message: 'Email inválido' }),
+  password: z.string({
+    required_error: 'Senha é obrigatório',
+    invalid_type_error: 'Senha inválida',
+  }),
+  cpf: z.string({
+    required_error: 'CPF é obrigatório',
+    invalid_type_error: 'CPF inválido',
+  }),
+  role: z
+    .string({
+      required_error: 'Cargo é obrigatório',
+      invalid_type_error: 'Cargo inválido',
+    })
+    .min(1, { message: 'Cargo deve ser selecionado' }),
+  sector: z
+    .string({
+      required_error: 'Setor é obrigatório',
+      invalid_type_error: 'Setor inválido',
+    })
+    .min(1, { message: 'Setor deve ser selecionado' }),
+  workScheduleId: z.string({
+    required_error: 'Escala de trabalho é obrigatória',
+    invalid_type_error: 'Escala de trabalho inválida',
+  }),
+  isActive: z.boolean().default(true),
+})
 
 type RegisterCollaboratorFormData = z.infer<typeof registerCollaboratorFormSchema>
 type useRegisterCollaboratorFormProps = {
-    onSubmit: VoidFunction
+  onSubmit: VoidFunction
 }
 
-export function useRegisterCollaboratorForm({ onSubmit }: useRegisterCollaboratorFormProps) {
-    const { formState, reset, register, handleSubmit } =
+export function useRegisterCollaboratorForm({
+  onSubmit,
+}: useRegisterCollaboratorFormProps) {
+  const { formState, reset, register, handleSubmit, watch, control } =
     useForm<RegisterCollaboratorFormData>({
       resolver: zodResolver(registerCollaboratorFormSchema),
     })
 
-    const { collaboratorService } = useApi()
-    async function handleFormSubmit(formData: RegisterCollaboratorFormData) {
-      const collaborator = formData
-      const response = await collaboratorService.registerCollaborator(collaborator)
-      if (response.isSuccess) {
-        reset()
-        onSubmit()
-      } else {
-        alert("Erro ao cadastrar colaborador")
-      }
+  const { collaborationService } = useApi()
+  const { showError, showSuccess } = useToast()
+  async function handleFormSubmit(formData: RegisterCollaboratorFormData) {
+    const { password, workScheduleId, ...collaboratorData } = formData 
+
+    const response = await collaborationService.registerCollaborator(
+      collaboratorData,
+      password,
+      workScheduleId,
+    )
+    console.log(response)
+    if (response.isSuccess) {
+      showSuccess('Colaborador cadastrado com sucesso')
+      onSubmit()
     }
-    return {
-      errors: formState.errors,
-      isSubmiting: formState.isSubmitting,
-      register,
-      handleSubmit: handleSubmit(handleFormSubmit),
+    if (response.isFailure) {
+      showError(response.errorMessage)
     }
+  }
+
+  return {
+    errors: formState.errors,
+    isSubmiting: formState.isSubmitting,
+    control,
+    register,
+    handleSubmit: handleSubmit(handleFormSubmit),
+  }
 }
