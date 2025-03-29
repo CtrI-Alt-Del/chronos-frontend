43d5c2ddbba18276d64f08e7e3e149189f638ea0
kaufon <kauanfvale@gmail.com>
2025-03-29 01:27:51 -0300

    🖥️ui: add tag

---
+++ src/ui/global/widgets/components/tag/index.tsx
@@ -0,0 +1,51 @@
+import { Chip } from '@heroui/react' 
+import { twMerge } from 'tailwind-merge'
+
+type TagProps = {
+  type: 'sucess' | 'danger' | 'warning'
+  size?: 'sm' | 'md' | 'lg'
+  children: string
+  className?: string
+}
+export const Tag = ({ type, size, children, className }: TagProps) => {
+  switch (type) {
+    case 'sucess':
+      return (
+        <Chip
+          color='success'
+          size={size}
+          className={twMerge('bg-opacity-20 text-green-600', className)}
+        >
+          {children}
+        </Chip>
+      )
+
+    case 'danger':
+      return (
+        <Chip
+          color='danger'
+          size={size}
+          className={twMerge('bg-opacity-20 text-red-600', className)}
+        >
+          {children}
+        </Chip>
+      )
+
+    case 'warning':
+      return (
+        <Chip
+          color='warning'
+          size={size}
+          className={twMerge('bg-opacity-20 text-yellow-500', className)}
+        >
+          {children}
+        </Chip>
+      )
+    default:
+      return (
+        <Chip color='success' className='bg-opacity-20 text-green-600'>
+          {children}
+        </Chip>
+      )
+  }
+}
