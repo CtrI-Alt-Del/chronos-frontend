f36da0bbdebd4573f511fd9cde0caaa00d505c35
eduardokuwaharajr57@gmail.com <eduardokuwaharajr57@gmail.com>
2025-04-03 21:36:54 -0300

    🐛 fix: Removing the access route from the individual profile of employees to any user

--- src/app/(private)/collaboration/collaborators/[collaboratorId]/page.tsx
+++ src/app/(private)/collaboration/collaborators/[collaboratorId]/page.tsx
@@ -1,9 +1,24 @@
 import type { NextParams } from '@/api/next/types'
 import { authActions, collaborationActions, workScheduleActions } from '@/server/next-safe-action'
 import { CollaboratorPage } from '@/ui/collaboration/widgets/pages/collaborator'
+import { notFound, redirect } from 'next/navigation'
 
 export default async function Page({ params }: NextParams<'collaboratorId'>) {
   const { collaboratorId } = await params
+
+  const currentProfile = await collaborationActions.getCollaboratorProfile()
+
+  if (!currentProfile ) {
+    throw new Error('/not-found')
+  }
+  if (
+    !currentProfile?.data?.collaborator.role.includes('admin') &&
+    !currentProfile?.data?.collaborator.role.includes('manager') &&
+    currentProfile?.data?.collaborator.id !== collaboratorId
+  ) {
+    redirect('/not-found')
+    
+  }
 
   const collaboratoResponse = await collaborationActions.getCollaborator({
     collaboratorId: collaboratorId,
@@ -17,10 +32,12 @@ export default async function Page({ params }: NextParams<'collaboratorId'>) {
   if (!collaboratoResponse?.data?.collaborator) return
   if (!weekScheduleResponse?.data?.weekSchedule) return
   if (!dayOffScheduleResponse?.data?.dayOffSchedule) return
+  if (!currentProfile?.data?.collaborator) return
 
   const collaborator = collaboratoResponse.data.collaborator
   const weekSchedule = weekScheduleResponse.data.weekSchedule
   const dayOffSchedule = dayOffScheduleResponse.data.dayOffSchedule
+  const currentCollaborator = currentProfile.data.collaborator
 
   return (
     <CollaboratorPage
