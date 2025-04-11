d7f8ba340163ac99fca022cb31e32908f1a2860b
kaufon <kauanfvale@gmail.com>
2025-04-10 21:17:21 -0300

    📟server: use Create Day Off Schedule Solicitation action

--- src/validation/schemas/solicitation/day-off-schedule-adjustment-solicitation-schema.ts
+++ src/validation/schemas/solicitation/day-off-schedule-adjustment-solicitation-schema.ts
@@ -1,15 +1,15 @@
 import {z} from "zod"
-import { workScheduleSchema } from "../work-schedule"
+import { daysOffScheduleSchema, workScheduleSchema } from "../work-schedule"
 import { responsibleAggregateSchema } from "../global/responsible-aggregate-schema"
 
 export const dayOffScheduleAdjustmentSolicitationSchema = z.object({
   id: z.string().optional(),
-  description: z.string(),
-  date: z.coerce.date(),
-  status: z.enum(["PENDING", "APPROVED", "DENIED"]),
-  feedbackMessage: z.string(),
-  senderResponsible: responsibleAggregateSchema,
+  description: z.string().optional(),
+  date: z.coerce.date().optional(),
+  status: z.enum(["PENDING", "APPROVED", "DENIED"]).optional(),
+  feedbackMessage: z.string().optional(),
+  senderResponsible: responsibleAggregateSchema.optional(),
   replierResponsible: responsibleAggregateSchema.optional(),
-  type: z.literal("WORK_SCHEDULE"),
-  dayOffSchedule: workScheduleSchema
+  type: z.literal("WORK_SCHEDULE").optional(),
+  dayOffScheduleDto: daysOffScheduleSchema
 })
