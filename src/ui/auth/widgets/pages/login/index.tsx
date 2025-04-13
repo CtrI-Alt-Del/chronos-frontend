a32f83237ae98ec04590d7a52675eaa6ecc53f41
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-04-13 16:54:53 +0000

    🐛 fix: form error handling in login page

--- src/ui/auth/widgets/pages/login/index.tsx
+++ src/ui/auth/widgets/pages/login/index.tsx
@@ -1,59 +1,61 @@
 'use client'
 
-import { Input } from "@heroui/input";
+import { Input } from '@heroui/input'
 
-import { useLoginPage } from "./use-login-page";
-import { AnimatedButton } from "./animated-button";
+import { useLoginPage } from './use-login-page'
+import { AnimatedButton } from './animated-button'
 
 export const LoginPage = () => {
-  const { errors, isSubmitting, handleSubmit, registerField } = useLoginPage();
+  const { formErrors, isSubmitting, handleSubmit, registerField } = useLoginPage()
 
   return (
-    <div className="mx-auto w-full max-w-md">
-      <div className="flex flex-col justify-center items-center mt-4 mb-2 md:mb-4">
-        <h1 className="text-2xl font-semibold md:text-3xl">Login</h1>
+    <div className='mx-auto w-full max-w-md'>
+      <div className='flex flex-col justify-center items-center mt-4 mb-2 md:mb-4'>
+        <h1 className='text-2xl font-semibold md:text-3xl'>Login</h1>
       </div>
 
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 md:gap-6">
-        <div className="flex flex-col gap-6">
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4 md:gap-6'>
+        <div className='flex flex-col gap-6'>
           <Input
-            label="Email"
+            label='Email'
             classNames={{
               inputWrapper:
-                "bg-zinc-200 focus-within:border-2 focus-within:border-primary",
-              input: "bg-transparent focus:outline-none text-sm md:text-base",
-              label: "text-sm md:text-base",
+                'bg-zinc-200 focus-within:border-2 focus-within:border-primary',
+              input: 'bg-transparent focus:outline-none text-sm md:text-base',
+              label: 'text-sm md:text-base',
             }}
-            labelPlacement="outside"
-            type="email"
-            variant="flat"
+            labelPlacement='outside'
+            type='email'
+            variant='flat'
             disabled={isSubmitting}
-            errorMessage={errors.email?.message}
-            {...registerField("email")}
+            isInvalid={Boolean(formErrors.email)}
+            errorMessage={formErrors.email?.message}
+            {...registerField('email')}
           />
           <Input
-            label="Senha"
+            label='Senha'
             classNames={{
               inputWrapper:
-                "bg-zinc-200 focus-within:border-2 focus-within:border-primary",
-              input: "bg-transparent focus:outline-none text-sm md:text-base",
-              label: "text-sm md:text-base",
+                'bg-zinc-200 focus-within:border-2 focus-within:border-primary',
+              input: 'bg-transparent focus:outline-none text-sm md:text-base',
+              label: 'text-sm md:text-base',
             }}
-            labelPlacement="outside"
-            type="password"
-            variant="flat"
+            labelPlacement='outside'
+            type='password'
+            variant='flat'
             disabled={isSubmitting}
-            errorMessage={errors.password?.message}
-            {...registerField("password")}
+            isInvalid={Boolean(formErrors.password)}
+            errorMessage={formErrors.password?.message}
+            {...registerField('password')}
           />
         </div>
-        <div className="overflow-hidden relative mb-10 rounded-full md:mb-12">
+        <div className='overflow-hidden relative mb-10 rounded-full md:mb-12'>
           <AnimatedButton isSubmitting={isSubmitting} showAnimation={false} />
         </div>
       </form>
-      <div className="text-xs text-center text-gray-400 md:text-sm">
+      <div className='text-xs text-center text-gray-400 md:text-sm'>
         made by Ctrl Alt Del
       </div>
     </div>
-  );
-};
+  )
+}
