21125d43634c7776048830bb1d220ac373917ae6
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-04-14 00:50:18 +0000

    🐛 fix: remove week schedule action in collaborator page

--- src/app/(private)/collaboration/collaborators/[collaboratorId]/page.tsx
+++ src/app/(private)/collaboration/collaborators/[collaboratorId]/page.tsx
@@ -6,25 +6,14 @@ export default async function Page({ params }: NextParams<'collaboratorId'>) {
   const collaboratoResponse = await collaborationActions.getCollaborator({
     collaboratorId: params.collaboratorId,
   })
-  const weekScheduleResponse = await workScheduleActions.getWeekSchedule({
-    collaboratorId: params.collaboratorId,
-  })
   const dayOffScheduleResponse = await workScheduleActions.getDayOffSchedule({
     collaboratorId: params.collaboratorId,
   })
   if (!collaboratoResponse?.data?.collaborator) return
-  if (!weekScheduleResponse?.data?.weekSchedule) return
   if (!dayOffScheduleResponse?.data?.dayOffSchedule) return
 
   const collaborator = collaboratoResponse.data.collaborator
-  const weekSchedule = weekScheduleResponse.data.weekSchedule
   const dayOffSchedule = dayOffScheduleResponse.data.dayOffSchedule
 
-  return (
-    <CollaboratorPage
-      collaborator={collaborator}
-      weekSchedule={weekSchedule}
-      dayOffSchedule={dayOffSchedule}
-    />
-  )
+  return <CollaboratorPage collaborator={collaborator} dayOffSchedule={dayOffSchedule} />
 }
