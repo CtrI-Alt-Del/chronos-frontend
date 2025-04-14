f01e59010c6bf3f4ab8807c70f87c38203e73900
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-04-14 00:53:37 +0000

    🐛 fix: create day off schedule calling in useDayOffScheduleTab

--- src/server/actions/work-schedule/index.ts
+++ src/server/actions/work-schedule/index.ts
@@ -1,5 +1,4 @@
 export { GetTodayWorkdayLogAction } from './get-today-workday-log-action'
 export { PunchTimeAction } from './punch-time-action'
 export { UpdateDayOffAction } from './update-day-off-schedule-action'
-export { UpdateWeekScheduleAction } from './update-week-schedule-action'
 export { GetDayOffScheduleAction } from './get-day-off-schedule-action'
