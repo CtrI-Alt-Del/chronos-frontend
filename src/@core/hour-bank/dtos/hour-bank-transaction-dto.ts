64d5072346b2ff758afdb6905602bc17315c2be1
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-04-17 23:55:28 +0000

    🛍️ dtos: add dtos for hour bank module

---
+++ src/@core/hour-bank/dtos/hour-bank-transaction-dto.ts
@@ -0,0 +1,7 @@
+export type HourBankTransactionDto = {
+  date: string
+  type: string
+  time: string
+  reason: string
+  operation: string
+}
---
+++ src/@core/hour-bank/dtos/index.ts
@@ -0,0 +1,2 @@
+export type { HourBankBalance } from './hour-bank-balance-dto'
+export type { HourBankTransactionDto } from './hour-bank-transaction-dto'
