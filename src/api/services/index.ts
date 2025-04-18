9b49b973dfc4ea6aad3c3f98c1c73af8fb19b878
kaufon <kauanfvale@gmail.com>
2025-04-18 18:08:37 -0300

    📶rest: add justification-type service

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -1,4 +1,5 @@
 export { AuthService } from './auth-service'
+export {JustificaionTypeService} from "./justification-type-service"
 export { CollaborationService } from './collaboration-service'
 export { SolicitationService } from './solicitation-service'
 export { WorkScheduleService } from './work-schedule-service'
