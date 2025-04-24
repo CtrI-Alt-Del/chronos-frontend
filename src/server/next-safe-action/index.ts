f9bbc812064087afda8004ecfc46a44f35b50119
0thigs <machinimavlogs@gmail.com>
2025-04-24 17:55:47 -0300

    📟server: include listJustificationTypes in solicitationActions for enhanced functionality

--- src/server/next-safe-action/index.ts
+++ src/server/next-safe-action/index.ts
@@ -21,6 +21,7 @@ import {
   createJustificationType,
   createTimePunchAdjustmentSolicitation,
   deleteJustificaionType,
+  listJustificationTypes,
   resolveSolicitation,
   updateJustificationType,
 } from './solicitation-actions'
@@ -31,6 +32,7 @@ const solicitationActions = {
   createJustificationType,
   deleteJustificaionType,
   updateJustificationType,
+  listJustificationTypes,
   createTimePunchAdjustmentSolicitation,
 }
 
