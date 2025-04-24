f756fdf38de960bff0a9ff3ec27f8d4013ff3d1d
EduardoKuwahara <eduardokuwaharajr57@gmail.com>
2025-04-24 13:16:21 -0300

    🖥️ ui: implementation of the hook on the hour bank page

--- src/ui/work-schedule/widgets/pages/hours-bank/index.tsx
+++ src/ui/work-schedule/widgets/pages/hours-bank/index.tsx
@@ -1,48 +1,32 @@
-'use client'
-
-import { useHoursBankPage } from './use-hours-bank-page'
+import { useHourBankPage } from './use-hours-bank-page'
 import { BalanceSummary } from './balance-summary'
 import { QuickActions } from './quick-actions'
-import { RecentHistory } from './recent-history'
+import { Transactions } from './transaction-history'
 import { DateDisplay } from './date-display'
 
-interface HoursBankPageProps {
+type HourBankPageProps = {
   collaboratorId: string
-  collaboratorName: string
 }
 
-export function HoursBankPage({ collaboratorId, collaboratorName }: HoursBankPageProps) {
-  const {
-    hoursBankData,
-    currentBalance,
-    hoursWorkedToday,
-    totalHoursWorked,
-    recentEntries,
-  } = useHoursBankPage(collaboratorId, new Date(), new Date())
+export const HourBankPage = ({ collaboratorId }: HourBankPageProps) => {
   const today = new Date()
 
   return (
     <div className='flex flex-col gap-4 py-4'>
       <DateDisplay date={today} />
-      
+
       <div className='flex gap-6'>
         <section className='flex-1'>
-          <BalanceSummary 
-            currentBalance={currentBalance}
-            hoursWorkedToday={hoursWorkedToday}
-            totalHoursWorked={totalHoursWorked}
-          />
+          <BalanceSummary collaboratorId={collaboratorId} />
           <div className='flex flex-col mt-8'>
             <QuickActions />
           </div>
         </section>
 
-        <section className='w-96'>
-          <RecentHistory entries={recentEntries} />
-        </section>
+        <section className='w-96'><Transactions colaboratorId={collaboratorId} /></section>
       </div>
     </div>
   )
 }
 
-export default HoursBankPage
+export default HourBankPage
