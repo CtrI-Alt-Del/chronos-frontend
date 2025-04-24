4248304f45f3058525466de2f9f5e822a66b9795
EduardoKuwahara <eduardokuwaharajr57@gmail.com>
2025-04-24 13:10:50 -0300

    📶 api: create the get to balance of hour bank

---
+++ src/server/actions/hours-bank/get-hour-bank-balance.ts
@@ -0,0 +1,27 @@
+import type { HourBankBalanceDto } from '@/@core/hour-bank/dtos/hour-bank-balance-dto'
+import type { HourBankService } from '@/@core/hour-bank/interfaces'
+import type { Action, Call } from '@/@core/global/interfaces/rpc'
+
+type Request = {
+  collaboratorId: string
+}
+
+type Response = {
+  hourBankBalance: HourBankBalanceDto
+}
+
+export const GetHourBankBalanceAction = (
+  service: HourBankService,
+): Action<Request, Response> => {
+  return {
+    async handle(call: Call<Request>) {
+      const { collaboratorId } = call.getRequest()
+      const response = await service.calculateHourBankBalance(collaboratorId)
+      if (response.isFailure) response.throwError()
+
+      return {
+        hourBankBalance: response.body,
+      }
+    },
+  }
+}
