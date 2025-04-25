6e2f8ca355a0006d088215dd07d5cc106630a0c8
kaufon <kauanfvale@gmail.com>
2025-04-25 19:37:00 -0300

    ♻️refactor: hour bank summary

--- src/ui/hour-bank/widgets/components/balance-summary/index.tsx
+++ src/ui/hour-bank/widgets/components/balance-summary/index.tsx
@@ -1,20 +1,24 @@
 import { hourBankActions } from '@/server/next-safe-action'
-import { useHourBankSummary } from './use-hour-bank-summary'
-import { HourBankBalanceDto } from '@/@core/hour-bank/dtos/hour-bank-balance-dto'
+import type { HourBankBalanceDto } from '@/@core/hour-bank/dtos/hour-bank-balance-dto'
+import type { WorkTimeDto } from '@/@core/work-schedule/dtos'
 
 type BalanceSummaryProps = {
   hourBankBalance: HourBankBalanceDto
-  hoursWorkedToday: string
-  totalHoursWorked:string
+  workTime: WorkTimeDto
+  collaboratorName?: string
 }
 
-export const BalanceSummary = ({ hourBankBalance,hoursWorkedToday,totalHoursWorked }: BalanceSummaryProps) => {
-
+export const BalanceSummary = ({
+  hourBankBalance,
+  workTime,
+  collaboratorName,
+}: BalanceSummaryProps) => {
   return (
     <div className='flex flex-col p-10 rounded-2xl border border-gray-border'>
+      {collaboratorName && <h2 className='text-4xl font-semibold pb-6'>{collaboratorName}</h2>}
       <h2 className='text-3xl font-semibold'>Saldo de horas</h2>
       <p className='text-xl font-normal mt-3 text-[#7C7C7C]'>
-        Resumo do seu banco de horas atual
+        {collaboratorName ? 'Banco de horas do colaborador' : 'Resumo do seu saldo banco de horas'}
       </p>
       <div className='flex flex-col items-start mt-16'>
         <div className='flex flex-col items-center'>
@@ -28,11 +32,11 @@ export const BalanceSummary = ({ hourBankBalance,hoursWorkedToday,totalHoursWork
       <div className='flex flex-row gap-8 mt-16'>
         <div className='flex-col flex-1 items-start p-5 bg-blue-100 rounded-2xl border border-[#D5E7FF]'>
           <p className='text-gray-500'>Horas trabalhadas hoje</p>
-          <p className='mt-5 text-5xl font-semibold'>{hoursWorkedToday}</p>
+          <p className='mt-5 text-5xl font-semibold'>{workTime.workdayTime}</p>
         </div>
         <div className='flex-col flex-1 items-start p-5 bg-blue-100 rounded-2xl border border-[#D5E7FF]'>
-          <p className='text-gray-500'>Total de horas trabalhadas</p>
-          <p className='mt-5 text-5xl font-semibold'>{ totalHoursWorked }</p>
+          <p className='text-gray-500'>Total de horas trabalhadas no mes</p>
+          <p className='mt-5 text-5xl font-semibold'>{workTime.workMonthTime}</p>
         </div>
       </div>
     </div>
--- src/ui/hour-bank/widgets/pages/hour-bank/index.tsx
+++ src/ui/hour-bank/widgets/pages/hour-bank/index.tsx
@@ -1,33 +1,49 @@
-"use client"
+'use client'
 import type { HourBankBalanceDto } from '@/@core/hour-bank/dtos/hour-bank-balance-dto'
 import { BalanceSummary } from '../../components/balance-summary'
 import { DateDisplay } from '../../components/date-display'
 import { QuickActions } from '../../components/quick-actions'
 import { Transactions } from '../../components/transaction-history'
 import { useHourBankPage } from './use-hours-bank-page'
+import type { WorkTimeDto } from '@/@core/work-schedule/dtos'
+import { CollaboratorDto } from '@/@core/collaboration/dtos'
 
 type HourBankPageProps = {
-  collaboratorId: string
+  collaborator: CollaboratorDto
   hourBankBalance: HourBankBalanceDto
+  workTime: WorkTimeDto
+  isCollaboratorOwnHourBank?: boolean
 }
 
-export const HourBankPage = ({ collaboratorId,hourBankBalance }: HourBankPageProps) => {
-  const {today} = useHourBankPage(collaboratorId)
+export const HourBankPage = ({
+  collaborator,
+  hourBankBalance,
+  workTime,
+  isCollaboratorOwnHourBank,
+}: HourBankPageProps) => {
+  const { today } = useHourBankPage(collaborator.id as string)
 
   return (
     <div className='flex flex-col gap-4 py-4'>
       <DateDisplay date={today} />
-
       <div className='flex gap-6'>
         <section className='flex-1'>
-          <BalanceSummary hourBankBalance={hourBankBalance} hoursWorkedToday='08:00' totalHoursWorked='08:32'/>
-          <div className='flex flex-col mt-8'>
-            <QuickActions />
-          </div>
+          <BalanceSummary
+            hourBankBalance={hourBankBalance}
+            workTime={workTime}
+            collaboratorName={
+              isCollaboratorOwnHourBank === false ? collaborator.name : undefined
+            }
+          />
+          {isCollaboratorOwnHourBank && (
+            <div className='flex flex-col mt-8'>
+              <QuickActions />
+            </div>
+          )}
         </section>
 
         <section className='w-96'>
-          <Transactions colaboratorId={collaboratorId} />
+          <Transactions colaboratorId={collaborator.id as string} />
         </section>
       </div>
     </div>
