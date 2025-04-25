ad30919ca9023a77e10069dbd6cf1b31692f718f
kaufon <kauanfvale@gmail.com>
2025-04-25 19:35:09 -0300

    🌐domain: add new method to work schedule service

--- src/@core/work-schedule/dtos/index.ts
+++ src/@core/work-schedule/dtos/index.ts
@@ -1,4 +1,5 @@
 export type { TimePunchDto } from './time-punch-dto'
+export type {WorkTimeDto} from "./work-time-dto"
 export type { WeekdayScheduleDto } from './weekday-schedule-dto'
 export type { WorkScheduleDto } from './work-schedule-dto'
 export type { WorkdayLogDto } from './workday-log-dto'
