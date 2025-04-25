dbca2bb79da75ad47a29b4454bfa80d2b44954a9
kaufon <kauanfvale@gmail.com>
2025-04-24 22:12:11 -0300

    🪨constants: add create-day-off-solicitation schema

---
+++ src/server/actions/solicitation/create-day-off-solicitation.ts
@@ -0,0 +1,33 @@
+import type { Action } from '@/@core/global/interfaces/rpc'
+import type { ISolicitationService } from '@/@core/solicitation/interfaces'
+import { ROUTES } from '@/constants'
+import type { Call } from '@/@core/global/interfaces/rpc'
+type RequestBody = {
+  dayOff: string
+  justificationType: {
+    id: string
+    name: string
+    needsAttachment: string
+  }
+  description: string
+  file?: File
+}
+export const CreateDayOffSolicitationAction = (
+  service: ISolicitationService,
+): Action<RequestBody> => {
+  return {
+    async handle(call: Call<RequestBody>) {
+      const solicitation = call.getRequest()
+      const response = await service.createDayOffSolicitation(
+        solicitation.dayOff,
+        solicitation.justificationType.id,
+        solicitation.description,
+        solicitation.justificationType.name,
+        solicitation.justificationType.needsAttachment,
+        solicitation.file,
+      )
+      if (response.isFailure) response.throwError()
+      call.redirect(ROUTES.solicitation.solicitations)
+    },
+  }
+}
