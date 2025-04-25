1fa64e9fad16abf903c2ca662d13c3637584ae4d
kaufon <kauanfvale@gmail.com>
2025-04-24 22:08:09 -0300

    ♻️refactor: create day off solicitation in service

--- src/@core/solicitation/interfaces/solicitation-service.ts
+++ src/@core/solicitation/interfaces/solicitation-service.ts
@@ -11,13 +11,22 @@ export interface ISolicitationService {
   createDayOffScheduleAdjustmentSolicitation(
     solicitation: DayOffScheduleAdjustmentSolicitationDto,
   ): Promise<ApiResponse<void>>
+  createDayOffSolicitation(
+      dayOff: string ,
+      justificationTypeId: string,
+      description: string,
+      justificationTypeName: string ,
+      justificationTypeShouldHaveAttachment: string,
+      attachment?: File
+    
+  ):Promise<ApiResponse<void>>
   createTimePunchLogAdjustmentSolicitation(
     solicitation: TimePunchLogAdjustmentSolicitationDto,
   ): Promise<ApiResponse<void>>
   resolveSolicitation(
     solicitationId: string,
     action: 'APPROVED' | 'DENIED',
-    solicitationType: 'DAY_OFF_SCHEDULE' | 'TIME_PUNCH',
+    solicitationType: 'DAY_OFF_SCHEDULE' | 'TIME_PUNCH' | 'DAY_OFF',
   ): Promise<ApiResponse<void>>
   listJustificationTypes(): Promise<ApiResponse<JustificationTypeDto[]>>
   createJustificationType(
