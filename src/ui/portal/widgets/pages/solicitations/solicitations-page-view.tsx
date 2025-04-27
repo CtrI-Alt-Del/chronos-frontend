00838f83c609b3301c2022298d5b63f5a3310674
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-04-27 22:18:12 +0000

    🖥️ ui: add more tabs in solicitations page

--- src/ui/portal/widgets/pages/solicitations/solicitations-page-view.tsx
+++ src/ui/portal/widgets/pages/solicitations/solicitations-page-view.tsx
@@ -6,6 +6,15 @@ export const SolicitationsPageView = () => {
     <div>
       <div className='px-10 py-4'>
         <Tabs.Container>
+          <Tabs.Item
+            key='excused-absence'
+            title={<Tabs.Title>Abono de falta</Tabs.Title>}
+          >
+            <PaidOvertimeSolicitationsAccordion />
+          </Tabs.Item>
+          <Tabs.Item key='day-off' title={<Tabs.Title>Folga</Tabs.Title>}>
+            <PaidOvertimeSolicitationsAccordion />
+          </Tabs.Item>
           <Tabs.Item key='paid-overtime' title={<Tabs.Title>Hora extra</Tabs.Title>}>
             <PaidOvertimeSolicitationsAccordion />
           </Tabs.Item>
