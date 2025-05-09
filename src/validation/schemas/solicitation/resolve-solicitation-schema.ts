f1af80de99bc3523da93331c6c55dc3ebb1edd2d
0thigs <machinimavlogs@gmail.com>
2025-05-08 22:54:04 -0300

    🖥️ui: refactor solicitation and work schedule schemas to use global validation schemas

--- src/validation/schemas/solicitation/resolve-solicitation-schema.ts
+++ src/validation/schemas/solicitation/resolve-solicitation-schema.ts
@@ -1,10 +1,11 @@
 import { z } from 'zod'
 import { responsibleAggregateSchema } from '../global/responsible-aggregate-schema'
+import { idSchema, feedbackMessageSchema } from '@/validation/schemas/global'
 
 export const resolveSolicitationSchema = z.object({
-  id: z.string().optional(),
+  id: idSchema.optional(),
   status: z.enum(['DENIED', 'APPROVED']),
-  feedbackMessage: z.string().optional(),
-  type: z.enum(['DAY_OFF_SCHEDULE', 'TIME_PUNCH',"DAY_OFF"]),
-  collaboratorId: z.string()
+  feedbackMessage: feedbackMessageSchema,
+  type: z.enum(['DAY_OFF_SCHEDULE', 'TIME_PUNCH', 'DAY_OFF']),
+  collaboratorId: idSchema,
 })
