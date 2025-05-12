788fb4f56ae021e32259c199156b4c7a27119e5e
kaufon <kauanfvale@gmail.com>
2025-05-12 16:12:25 -0300

    🖥️ui: add date range calendar

---
+++ src/ui/global/widgets/components/date-range-calendar/index.tsx
@@ -0,0 +1,28 @@
+import { RangeCalendar } from '@heroui/react'
+import { parseDate, type CalendarDate } from '@internationalized/date'
+
+type Props = {
+  days: string[] 
+}
+
+export const DateRangeCalendar = ({ days }: Props) => {
+  const parsedDays: CalendarDate[] = days.map((d) => parseDate(d))
+
+  const minDate = parsedDays.reduce((a, b) => (a.compare(b) < 0 ? a : b))
+  const maxDate = parsedDays.reduce((a, b) => (a.compare(b) > 0 ? a : b))
+
+  return (
+    <div className='relative'>
+      <RangeCalendar
+        aria-label='Dias'
+        defaultValue={{
+          start: minDate,
+          end: maxDate,
+        }}
+        isReadOnly
+        className='pointer-events-none opacity-70'
+      />
+    </div>
+  )
+}
+
