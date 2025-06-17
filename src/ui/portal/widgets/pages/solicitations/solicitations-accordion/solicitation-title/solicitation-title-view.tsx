5874d072a5255e7d80be9f15c746b64e06a4fc3f
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-06-17 01:00:06 -0300

    🐛 fix: work leave solicitation title in solicitations page

--- src/ui/portal/widgets/pages/solicitations/solicitations-accordion/solicitation-title/solicitation-title-view.tsx
+++ src/ui/portal/widgets/pages/solicitations/solicitations-accordion/solicitation-title/solicitation-title-view.tsx
@@ -7,7 +7,7 @@ const TITLES: Record<string, string> = {
   day_off: 'Pedido de folga',
   excused_absence: 'Abono de falta',
   paid_overtime: 'Hora extra remunerada',
-  work_leave: 'Ausência do trabalho',
+  work_leave: 'Férias ou Afastamento',
   vacation: 'Ferias',
 }
 
